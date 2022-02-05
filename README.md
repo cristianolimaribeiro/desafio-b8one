bc5b9679762e2343f6b6ecef975ef18f7e8a7e47
cristianolimaribeiro <cristiano.lima.ribeiro@gmail.com>
2022-02-04 21:38:33 -0300

    prova b8one academy

---
+++ README.md
@@ -0,0 +1,16 @@
+
+# Teste prático B8one Academy
+
+Projeto Teste final para B8one Academy
+
+
+
+## Em que consistiu o desafio
+
+Foi idealizado para avaliar os conhecimentos seguindo uma trajetória onde se avaliava a perfomance e a agilidade com que se consegue desenvolver um projeto sendo disponibilidade um mockup no figma como orientação 
+## Stack utilizada
+
+**Front-end:** HTML, CSS e JAVASCRIPT
+
+
+
---
+++ assets/js/script.js
@@ -0,0 +1,50 @@
+let counter = document.querySelector('#number_counter')
+let btnPlus = document.querySelector('#plus')
+let btnMinus = document.querySelector('#minus')
+
+let counterVal = 1
+counter.innerText = counterVal
+
+setInterval(function () {
+   responsiveWindow()
+}, 1000);
+
+function incrementValue(){
+    if(counterVal === 3){
+        btnPlus.src = './assets/images/plus_empty.svg'
+    } else {
+        counterVal++
+        btnPlus.src = './assets/images/plus.svg'
+        btnMinus.src = './assets/images/minus.svg'
+       
+    }
+    counter.innerText = counterVal
+}
+
+function decrementValue(){
+    if(counterVal === 1){
+        btnMinus.src = './assets/images/minus_empty.svg'
+    } else {
+        counterVal--
+        btnPlus.src = './assets/images/plus.svg'
+        btnMinus.src = './assets/images/minus.svg'
+    }
+    counter.innerText = counterVal
+}
+function responsiveWindow(){
+    if(window.screen.width >= 1366){
+        document.querySelector('#search').placeholder="What are you looking for?"
+        document.querySelector('.bag').innerHTML = `<a href="#" id="user"><img src="./assets/images/user.svg" alt="user"><span>login</span></a>
+                                                    <a href="#" id="wishlist"><img src="./assets/images/heart.svg" alt="wishlist"><span>wishlist</span></a>
+                                                    <a href="#" id="bag"><img src="./assets/images/bag.svg" alt="bag"></a>`
+    }else {
+        document.querySelector('#search').placeholder=" "
+        document.getElementById('user').remove()
+        document.getElementById('wishlist').remove()
+    }
+    
+}
+
+btnPlus.addEventListener('click', incrementValue)
+btnMinus.addEventListener('click', decrementValue)
+
